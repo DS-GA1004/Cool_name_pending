a79b5a03ecd3422a564fa86f7b10d5137f1bb3d3
Heejong Kim <heejong.hj.kim@gmail.com>
2018-04-26 20:44:12 -0400

    make mutual information in function

--- codes/mutual_information.py
+++ codes/mutual_information.py
@@ -12,31 +12,21 @@ from pyspark.sql.functions import col
 import datetime
 
 
-
-# Read data in data frame format
-dt = spark.read.format('csv').options(header='true',inferschema='true').load("file:///home/hk2451/project/cleaned/cleaned311.csv")
-
-attrIn1 = dt.select('Agency').distinct().rdd.flatMap(lambda x: x).collect()
-attrIn2 = dt.select('Community_Board').distinct().rdd.flatMap(lambda x: x).collect()
-
-totalRow = dt.count()
-
-pXY = np.zeros((len(attrIn1), len(attrIn2)))
-pX = np.zeros((len(attrIn1),1))
-pY = np.zeros((len(attrIn2), 1))
-
-for i in range(0,len(attrIn1)):
-	for j in range(i,len(attrIn2)):
-        pXY[i][j] = (dt.filter(dt["Agency"] == attrIn1[i]).filter(dt["Community_Board"] == attrIn2[j]).count())/totalRow
-
-for i in range(0,len(attrIn1)):
-    pX[i] = (dt.filter(dt["Agency"] == attrIn1[i]).count())/totalRow
-
-for i in range(0,len(attrIn2)):
-    pY[i] = (dt.filter(dt["Community_Board"] == attrIn2[i]).count())/totalRow
-
-pXpY = pX.dot(pY.transpose())
-
-mask = pXY != 0
-
-MI = np.sum(np.multiply(pXY[mask], np.log(np.divide(pXY[mask], pXpY[mask]))))
+def mutual_information(dt, attr1, attr2):	
+	attrIn1 = dt.select(attr1).distinct().rdd.flatMap(lambda x: x).collect()
+	attrIn2 = dt.select(attr2).distinct().rdd.flatMap(lambda x: x).collect()
+	totalRow = dt.count()
+	pXY = np.zeros((len(attrIn1), len(attrIn2)))
+	pX = np.zeros((len(attrIn1),1))
+	pY = np.zeros((len(attrIn2), 1))
+	for i in range(0,len(attrIn1)):
+		for j in range(i,len(attrIn2)):
+			pXY[i][j] = (dt.filter(dt[attr1] == attrIn1[i]).filter(dt[attr2] == attrIn2[j]).count())/totalRow
+	for i in range(0,len(attrIn1)):
+	    pX[i] = (dt.filter(dt[attr1] == attrIn1[i]).count())/totalRow
+	for i in range(0,len(attrIn2)):
+	    pY[i] = (dt.filter(dt[attr2] == attrIn2[i]).count())/totalRow
+	pXpY = pX.dot(pY.transpose())
+	mask = pXY != 0
+	MI = np.sum(np.multiply(pXY[mask], np.log(np.divide(pXY[mask], pXpY[mask]))))
+	return pXY, pXpY, MI
