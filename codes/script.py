9342a232804c840efef5223160cde6b85912484d
Heejong Kim <heejong.hj.kim@gmail.com>
2018-04-30 14:16:32 -0400

    Update script.py

--- codes/script.py
+++ codes/script.py
@@ -82,11 +82,112 @@ df2 = df2.withColumn('time24',split(col('time'), ':')[0].cast("int")).drop('time
 # # latitude, longitude,etc address related attributes -> deleted
 df2 = df2.drop('longitude','latitude','location','cross_street_name','off_street_name')
 
+df2.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("collision_cleaned")
 
 ##################################
-# cleaning : taxi 
+# cleaning : weather & property
 ##################################
+# Change the filename below
+filename = "file:///home/hk2451/project/data/weather/weather-2011-2017.csv"
+dt = spark.read.format('csv').options(header='true',inferschema='true').load(filename)
 
+# Change name of the header (fill in black space with ‘_’ and delete ‘()’)
+exprs0 = [col(column).alias(column.strip()) for column in dt.columns]
+df = dt.select(*exprs0)
+exprs1 = [col(column).alias(column.replace(' ', '_')) for column in dt.columns]
+df = df.select(*exprs1)
+exprs2 = [col(column).alias(column.replace('(', '')) for column in df.columns]
+df = df.select(*exprs2)
+exprs3 = [col(column).alias(column.replace(')', '')) for column in df.columns]
+df = df.select(*exprs3)
+df = df.toDF(*[c.lower() for c in df.columns])
+
+
+# Finding Null value (if the attribute has 90% null value, delete the attribute)
+totL = df.count()
+attrF = []
+for colName in df.columns:
+	attrF.append(df.filter(str(colName)+" is null").count()/totL)
+
+df = delete_useless_column(attrF, df, 0.9)
+
+# Finding Unspecified (if the attribute has over 90% unspecified, will delete)
+attrF = []
+for colName in df.columns:
+	attrF.append(df.filter(df[colName].like("%Unspecified%")).count()/totL)
+
+df = delete_useless_column(attrF, df, 0.9)
+
+# Date Transformation
+if ("weather" in filename):
+	# weather data
+	df2 = df.withColumn('date', col('date').cast(StringType()))
+	df2 = df2.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('date', 'yyyymmdd')))).drop('date')
+
+else if ("311" in filename):
+	df2 = df.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('created_date', 'MM/dd/yyyy'))))
+	df2 = df2.withColumn('time', col('created_date').substr(12, 11)).drop("created_date")
+
+else if ("property" in filename):
+	df2 = df.withColumn("yymmdd", to_date(from_unixtime(unix_timestamp('year', 'yyyy/mm'))))
+
+else:
+	df2 = df.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('date', 'MM/dd/yyyy')))).drop('date')
+
+# Map time (HH:MM) into 24 hours
+if ("311" in filename):
+	df2 = df2.withColumn('time24', when(split(col("time"), " ")[1]=="AM", split(col("time"), ':')[0].cast("int")).otherwise(split(col("time"), ':')[0].cast("int")+12))
+	df2 = df2.drop("time")
+
+if ("weather" in filename):
+	df2 = df2.withColumn('time24', floor(df2.time/100) ).drop('time')
+	
+else if ("property" not in filename):
+	df2 = df2.withColumn('time24',split(col("time"), ':')[0].cast("int")).drop('time')
+
+
+# Date into day only
+if ("property" not in filename):
+	df2 = df2.withColumn('day', dayofmonth("yymmdd"))
+
+# Date into month only
+df2 = df2.withColumn('month', month("yymmdd"))
+
+# Date into year only
+df2 = df2.withColumn('year', year("yymmdd"))
+
+
+# Additional file cleaning (file specific)
+if ("weather" in filename):
+	# drop column with 90% 999 999.99 ..etc useless rows
+	df2 = df2.drop('sd','sdw','sa')
+	df2 = df2.withColumn('spd_floor',floor(df2.spd)).drop('spd')
+	df2 = df2.withColumn('temp_floor',floor(df2.temp)).drop('temp')
+	df2 = df2.withColumn('prcp_floor',floor(df2.prcp)).drop('prcp')
+	df2 = df2.filter(df2.visb != '      ')
+
+df2.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("weather_cleaned")
+
+
+##################################
+# cleaning : taxi
+##################################
+dt = spark.read.format('csv').options(header='true',inferschema='true').load("file:///home/hk2451/project/data/taxi/yellow_tripdata_2017-01.csv")
+# Trip duration
+timeFmt = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+dt = dt.withColumn('tripTime',unix_timestamp(dt.tpep_dropoff_datetime,format=timeFmt)-unix_timestamp(dt.tpep_pickup_datetime,format=timeFmt))
+dt = dt.drop('tpep_dropoff_datetime')
+
+# Pickup datetime as date / time
+dt = dt.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('tpep_pickup_datetime', 'MM/dd/yyyy'))))
+# Day only
+dt = dt.withColumn('day', dayofmonth("yymmdd"))
+# Date into month only
+dt = dt.withColumn('month', month("yymmdd"))
+# Date into year only
+dt = dt.withColumn('year', year("yymmdd"))
+# Time in 24 
+dt = dt.withColumn('time24', date_format(from_unixtime(unix_timestamp('tpep_pickup_datetime')), 'HH')).drop('tpep_pickup_datetime')
 
 
 ##################################
@@ -114,11 +215,7 @@ for colab in colAB:
 dfJoinF12 = dfJoin.filter(col('year') == 2012)
 dfJoinF12.count()
 
-dfJoinF12.createOrReplaceTempView("DT")
-
 dfJoinF12.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("join_year_2012_2")
-
-#spark.sql("SELECT * FROM DT").coalesce(1).write.save("join_year_2012", format="csv", header="true")
 
 ##################################
 # Mutual Information
@@ -135,7 +232,6 @@ MI = mi.mutual_information(dt, attr1, attr2, uniqueAttr,saveFlag,saveName)
 
 # If you don't need to save it
 MI = mi.mutual_information(dt, attr1, attr2, uniqueAttr,0,saveName)
-
 
 
 
