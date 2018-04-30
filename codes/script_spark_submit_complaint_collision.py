f1fb12dfca2e02f2f9989a2ec62e79b2d52f1a90
Heejong Kim <heejong.hj.kim@gmail.com>
2018-04-29 22:23:17 -0400

    Update script_spark_submit_complaint_collision.py

--- codes/script_spark_submit_complaint_collision.py
+++ codes/script_spark_submit_complaint_collision.py
@@ -23,21 +23,56 @@ sc.addFile("/home/hk2451/project/Cool_name_pending/codes/mutual_information.py")
 import cleaning_io as clean
 import mutual_information as mi
 
-dt = sqlCtx.read.format('csv').options(header='true',inferschema='true').load("/user/hk2451/join_year_2012_2")
+df1 = sqlCtx.read.format('csv').options(header='true',inferschema='true').load("complain_cleaned")
+df2 = sqlCtx.read.format('csv').options(header='true',inferschema='true').load("collision_cleaned")
 
-uniqueAttr = 'unique_key'
-attributes = dt.columns
-attributes.remove(uniqueAttr)
-MImat = np.zeros((len(attributes), len(attributes)))
+##################################
+# Inner Join and filter by year
+##################################
+# find common name column and change it 
 
-for i in range(0,len(attributes)):
-	for j in range(i+1,len(attributes)):
-		attr1 = attributes[i]
-		attr2 = attributes[j]
-		MImat[i][j] = mi.mutual_information(dt, attr1, attr2, uniqueAttr,0,'')
+colA = df1.columns
+colB = df2.columns
+colAB = list(set(colA).intersection(colB))
 
-rdd1 = sc.parallelize(MImat)
-rdd2 = rdd1.map(lambda x: [float(i) for i in x])
-MImatdf = rdd2.toDF(attributes)
-MImatdf.write.format("com.databricks.spark.csv").option("delimiter",",").save("collision_complain_2012-1")
+for colab in colAB:
+	df1 = df1.withColumnRenamed(colab,colab+'A')
 
+a = df1.alias('a')
+b = df2.alias('b')
+
+dfJoin = a.join(b, a.yymmddA == b.yymmdd, 'inner').select([col('a.'+xx) for xx in a.columns] + [col('b.'+xx) for xx in b.columns])
+# Delete duplicated columns
+
+for colab in colAB:
+	dfJoin = dfJoin.drop(colab+'A')
+
+##################################
+# Mutual information (Year by year)
+##################################
+# find common name column and change it 
+
+years = dfJoin.select('year').distinct().rdd.map(lambda x: x[0]).collect()
+years.remove(2012)
+
+for k in range(0, len(years)):
+        dfJoin = dfJoin.filter(col('year') == years[k])
+
+        uniqueAttr = 'unique_key'
+        attributes = dfJoin.columns
+        attributes.remove(uniqueAttr)
+        MImat = np.zeros((len(attributes), len(attributes)))
+
+        for i in range(0,len(attributes)):
+                for j in range(i+1,len(attributes)):
+                        print("row,col:",i,j)
+                        attr1 = attributes[i]
+                        attr2 = attributes[j]
+                        MImat[i][j] = mi.mutual_information(dfJoin, attr1, attr2, uniqueAttr,0,'')
+
+        rdd1 = sc.parallelize(MImat)
+        rdd2 = rdd1.map(lambda x: [float(i) for i in x])
+        MImatdf = rdd2.toDF(attributes)
+        MImatdf.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("complain_collision"+str(years[k]))
+        
+sc.stop()
