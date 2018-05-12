bcaca048b4412a011b14017f6043719bde02adb1
Heejong Kim <heejong.hj.kim@gmail.com>
2018-05-12 15:03:21 -0400

    Update script.py

--- codes/script.py
+++ codes/script.py
@@ -12,22 +12,16 @@ from pyspark.sql.functions import col
 from pyspark import SparkFiles
 import datetime
 
-
 sc.addFile("/home/hk2451/project/Cool_name_pending/codes/cleaning/cleaning_io.py")
 sc.addFile("/home/hk2451/project/Cool_name_pending/codes/mutual_information.py")
 #SparkFiles.get("file:///home/hk2451/project/Cool_name_pending/codes/cleaning/cleaning_io.py")
 import cleaning_io as clean
 import mutual_information as mi
 
-dt1 = spark.read.format('csv').options(header='true',inferschema='true').load("file:///home/hk2451/project/data/311/311_Service_Requests_from_2010_to_Present.csv")
-dt2 = spark.read.format('csv').options(header='true',inferschema='true').load("file:///home/hk2451/project/data/collision/NYPD_Motor_Vehicle_Collisions.csv")
-
-# cleanJoinF12 = spark.read.format('csv').options(header='true',inferschema='true').load("/user/hk2451/join_year_2012_2")
-
-
 ##################################
 # cleaning : complaint 
 ##################################
+dt1 = spark.read.format('csv').options(header='true',inferschema='true').load("file:///home/hk2451/project/data/311/311_Service_Requests_from_2010_to_Present.csv")
 df1 = clean.fixColumnName(dt1)
 df1 = clean.deleteNullAttr(df1)
 df1 = clean.zipcodeFilter(df1, 'incident_zip')
@@ -41,8 +35,6 @@ df1 = df1.withColumn('year', year("yymmdd"))
 df1 = df1.withColumn('time', col('created_date').substr(12, 11)).drop("created_date")
 df1 = df1.withColumn('time24', when(split(col("time"), " ")[1]=="AM", split(col("time"), ':')[0].cast("int")).otherwise(split(col("time"), ':')[0].cast("int")+12))
 df1 = df1.drop("time")
-# Unique key is same as number or lines -> deleted
-#df1 = df1.drop('unique_key')
 # Since we have ***Street Name***:
 # # Cross Street1,2, Intersection Street1,2, ***incident address***-> deleted 
 df1 = df1.drop('cross_street_1','cross_street_2','intersection_street_1','intersection_street_2','incident_address')
@@ -61,10 +53,12 @@ df1 = df1.drop('resolution_description')
 df1 = df1.drop('closed_date','resolution_action_updated_date','due_date')
 # Complaint type has 237 distinct categories. Desciptor is a detailed version of complaint type, which has 1126 categories. -> deleted
 df1 = df1.drop('descriptor')
+df1.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("complain_cleaned")
 
 ##################################
 # cleaning : collision 
 ##################################
+dt2 = spark.read.format('csv').options(header='true',inferschema='true').load("file:///home/hk2451/project/data/collision/NYPD_Motor_Vehicle_Collisions.csv")
 df2 = clean.fixColumnName(dt2)
 df2 = clean.deleteNullAttr(df2)
 df2 = df2.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('date', 'MM/dd/yyy')))).drop('date')
@@ -77,12 +71,9 @@ df2 = df2.withColumn('year', year("yymmdd"))
 df2 = clean.dateTranform(df2, 'date')
 df2 = clean.zipcodeFilter(df2, 'zip_code')
 df2 = df2.withColumn('time24',split(col('time'), ':')[0].cast("int")).drop('time')
-# Unique key is same as number or lines -> deleted
-#df2 = df2.drop('unique_key')
 # Since we have zip code and address:
 # # latitude, longitude,etc address related attributes -> deleted
 df2 = df2.drop('longitude','latitude','location','cross_street_name','off_street_name')
-
 df2.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("collision_cleaned")
 
 ##################################
@@ -102,19 +93,12 @@ if ("weather" in filename):
 	# weather data
 	df2 = df.withColumn('date', col('date').cast(StringType()))
 	df2 = df2.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('date', 'yyyymmdd')))).drop('date')
-
-elif ("311" in filename):
-	df2 = df.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('created_date', 'MM/dd/yyyy'))))
-	df2 = df2.withColumn('time', col('created_date').substr(12, 11)).drop("created_date")
 elif ("property" in filename):
 	df2 = df.withColumn("yymmdd", to_date(from_unixtime(unix_timestamp('year', 'yyyy/mm')))).drop('year')
 else:
 	df2 = df.withColumn('yymmdd', to_date(from_unixtime(unix_timestamp('date', 'MM/dd/yyyy')))).drop('date')
 
 # Map time (HH:MM) into 24 hours
-if ("311" in filename):
-	df2 = df2.withColumn('time24', when(split(col("time"), " ")[1]=="AM", split(col("time"), ':')[0].cast("int")).otherwise(split(col("time"), ':')[0].cast("int")+12))
-	df2 = df2.drop("time")
 elif ("weather" in filename):
 	df2 = df2.withColumn('time24', floor(df2.time/100) ).drop('time')
 elif ("property" not in filename):
@@ -130,7 +114,6 @@ df2 = df2.withColumn('month', month("yymmdd"))
 # Date into year only
 df2 = df2.withColumn('year', year("yymmdd"))
 
-
 # Additional file cleaning (file specific)
 if ("weather" in filename):
 	# drop column with 90% 999 999.99 ..etc useless rows
@@ -139,6 +122,7 @@ if ("weather" in filename):
 	df2 = df2.withColumn('temp_floor',floor(df2.temp)).drop('temp')
 	df2 = df2.withColumn('prcp_floor',floor(df2.prcp)).drop('prcp')
 	df2 = df2.filter(df2.visb != '      ')
+	df2.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("weather_cleaned")
 
 if ("property" in filename):
 	# ['bble', 'b', 'block', 'lot', 'owner', 'bldgcl', 'taxclass', 'ltfront', 'ltdepth', 'ext', 'stories', 'fullval', 'avland', 'avtot', 'exland', 'extot',
@@ -162,10 +146,7 @@ if ("property" in filename):
 	df2 = df2.withColumn('avland2_floor', floor(df2.avland2 / 1000000)).drop('avland2')
 	df2 = df2.withColumn('avtot2_floor', floor(df2.avtot2 / 1000000)).drop('avtot2')
 	df2 = df2.withColumn('extot2_floor', floor(df2.extot2 / 1000000)).drop('extot2')
-
-
-df2.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("weather_cleaned")
-
+	df2.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("property_cleaned")
 
 ##################################
 # cleaning : taxi
@@ -290,7 +271,6 @@ if yearIdx in [2017]:
 
 # Change name of id -> unique_key
 df1 = df1.withColumnRenamed('id','unique_key')
-
 df1.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("bike_cleaned"+str(yearIdx))
 
 ##################################
@@ -331,52 +311,5 @@ df1 = df1.withColumn('year', year("yymmdd"))
 
 # Time 
 df1 = df1.withColumn('time24', split(col("cmplnt_fr_tm"),':')[0].cast("int")).drop("cmplnt_fr_tm")
-
 df1.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("crime_cleaned")
 
-##################################
-# Inner Join and filter by year
-##################################
-# find common name column and change it 
-
-colA = df1.columns
-colB = df2.columns
-colAB = list(set(colA).intersection(colB))
-
-for colab in colAB:
-	df1 = df1.withColumnRenamed(colab,colab+'A')
-
-a = df1.alias('a')
-b = df2.alias('b')
-
-dfJoin = a.join(b, a.yymmddA == b.yymmdd, 'inner').select([col('a.'+xx) for xx in a.columns] + [col('b.'+xx) for xx in b.columns])
-# Delete duplicated columns
-
-for colab in colAB:
-	dfJoin = dfJoin.drop(colab+'A')
-
-# Select year
-dfJoinF12 = dfJoin.filter(col('year') == 2012)
-dfJoinF12.count()
-
-dfJoinF12.write.format("com.databricks.spark.csv").option("header", "true").option("delimiter",",").save("join_year_2012_2")
-
-
-
-
-
-##################################
-# Mutual Information
-##################################
-
-attr1 = 'agency'
-attr2 = 'number_of_persons_injured'
-uniqueAttr = 'unique_key'
-saveFlag = 1
-saveName = 'mutual_trial'
-
-# If you want to save file
-MI = mi.mutual_information(dt, attr1, attr2, uniqueAttr,saveFlag,saveName)
-
-# If you don't need to save it
-MI = mi.mutual_information(dt, attr1, attr2, uniqueAttr,0,saveName)
